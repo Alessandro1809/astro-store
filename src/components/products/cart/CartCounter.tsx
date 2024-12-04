ff59a8a1e5d1afebc5ab0b24c77c15c9576b1fad
Alessandro1809 <dalessandro18dg@gmail.com>
2024-12-04 17:31:56 -0600

    agregando nanostores para ppersistencia del estado del carrito entre componente react

--- src/components/products/cart/CartCounter.tsx
+++ src/components/products/cart/CartCounter.tsx
@@ -1,10 +1,27 @@
+import { itemsIncart } from "@/store"
+import { useStore } from "@nanostores/react"
+import { useEffect } from "react";
+import { CartCoookiesClient } from '../../../utils/cart-cookies';
 
 export const CartCounter = () => {
+
+  const $itemsInCart = useStore(itemsIncart);
+
+useEffect(() => {
+
+  const cart = CartCoookiesClient.getCart();
+  itemsIncart.set(cart.length);
+
+},[])
+
   return (
     <a href="/cart" className="relative inlline-block">
-        <span className="absolute -top-2 -right-2 bg-green-500 rounded-full w-5 h-5 flex items-center justify-center text-white text-xs">
-            3
-        </span>
+
+      {($itemsInCart > 0 )&& 
+       (<span className="absolute -top-2 -right-2 bg-green-500 rounded-full w-5 h-5 flex items-center justify-center text-white text-xs">
+       {$itemsInCart}
+    </span>)}
+       
         <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24"><path fill="#000000" d="M7 22q-.825 0-1.412-.587T5 20t.588-1.412T7 18t1.413.588T9 20t-.587 1.413T7 22m10 0q-.825 0-1.412-.587T15 20t.588-1.412T17 18t1.413.588T19 20t-.587 1.413T17 22M5.2 4h16.5l-4.975 9H8.1L7 15h12v2H3.625L6.6 11.6L3 4H1V2h3.25z"></path></svg>
     </a>
   )
---
+++ src/store/cart.store.ts
@@ -0,0 +1,4 @@
+import { atom } from 'nanostores';
+
+
+export const itemsIncart = atom(0);
