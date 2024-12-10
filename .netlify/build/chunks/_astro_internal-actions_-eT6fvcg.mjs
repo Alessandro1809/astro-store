1fea9efa8a348b1914c8e57c5c122e166c117c8a
Alessandro1809 <dalessandro18dg@gmail.com>
2024-12-09 22:10:26 -0600

    agregando metatags al layout y slug page

--- .netlify/build/chunks/_astro_internal-actions_-eT6fvcg.mjs
+++
@@ -1,8 +0,0 @@
-import { c as createUpdateProduct, d as deleteProductImg } from './getProductsByPage.action_CuCmYGY3.mjs';
-
-const server = {
-  createUpdateProduct,
-  deleteProductImg
-};
-
-export { server };
